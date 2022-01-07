b24f715c21ca56c9ee3b1078e74b40e7cb7b15cd
Yi hao <yihao.puah@gmail.com>
2022-01-07 14:21:26 +0800

    com

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import "./App.css";
 import SortingVisualizer from "./SortingVisualizer/SortingVisualizer";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <SortingVisualizer></SortingVisualizer>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
     <Router basename="/">
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
+      <App />
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
